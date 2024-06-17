31f53b4e19a27e2a6f5ca66fa1573c52684790ff
laksha-s <21a53.laksha@sjec.ac.in>
2024-06-17 22:46:22 +0530

    Add modify try1.py

--- numberPlay.py
+++ numberPlay.py
@@ -1,7 +1,6 @@
-def printTriangle(rows):
-    for i in range(1, rows):
+def printTriangle(row):
+    for i in range(1, row):
         for j in range(1, i):
             print(j, end =" ")
         print()
 printTriangle(10)
-printTriangle(20)
